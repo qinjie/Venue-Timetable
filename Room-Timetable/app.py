c5f7cae1696f854448badcfb096f4f5b6e07d8b2
pnbao <pnbao96@gmail.com>
2017-12-16 11:56:54 +0800

    Merge all branches.

--- Room-Timetable/app.py
+++ Room-Timetable/app.py
@@ -1,11 +1,12 @@
 import json
-
+import datetime
+from time import mktime
 import pymysql
 from chalice import Chalice
 
-db_host = "ec2-13-228-113-29.ap-southeast-1.compute.amazonaws.com"
+db_host = "iot-centre-rds.crqhd2o1amcg.ap-southeast-1.rds.amazonaws.com"
 db_name = "room_timetable"
-db_username = "ecer"
+db_username = "root"
 db_password = "Soe7014Ece"
 
 
@@ -15,20 +16,82 @@ app = Chalice(app_name="room_timetable")
 @app.route("/venue/{venue_name}/date/{date}")
 def index(venue_name, date):
     result = []
+    conn = connect_database()
+    with conn.cursor() as cur:
+        cur.execute("select from_time, to_time, purpose, user.name, venue.name from booking \
+                    join user on booking.user_id = user.id\
+                    join venue on booking.venue_id = venue.id\
+                    where location = '{}' and approval=1 and bdate='{}'".format(venue_name,date))
+    for row in cur:
+        result.append({'start_time':str(row[0]), 'end_time':str(row[1]), 'purpose':row[2],'user':row[3],'venue_name':row[4]})
+    if result:
+        return json.dumps(result)
+    else:
+        return 'Venue booking not found'
+
+@app.route('/booking', methods=['POST'])
+def booking():
+    request = app.current_request
+    data = request.json_body
+
+    user_id = str(data['user_id'])
+    venue_id = str(data['venue_id'])
+    purpose = str(data['purpose'])
+    bdate = str(data['bdate'])
+    from_time = str(data['from_time'])
+    to_time = str(data['to_time'])
+    conn = connect_database()
+    try:
+        with conn.cursor() as cur:
+            cur.execute('INSERT INTO `booking`(`user_id`, `venue_id`, `purpose`, `bdate`, `from_time`, `to_time`) VALUES (%s,%s,%s,%s,%s,%s)',(user_id, venue_id, purpose,bdate, from_time,to_time))
+            conn.commit()
+    except Exception as e:
+        return 'Booking Fail: ' + e.message
+    return 'Booking Success'
+
+@app.route('/booking/cancel', methods=['POST'])
+def canceling():
+    request = app.current_request
+    data = request.json_body
+
+    user_id = str(data['user_id'])
+    booking_id = str(data['booking_id'])
+    conn = connect_database()
+
+    isAdmin = False
+    with conn.cursor() as cur:
+        cur.execute('SELECT * FROM `user` where `id`=%s and `role`=20',(user_id))
+    for row in cur:
+        if row:
+            isAdmin = True
+            break
+
+    if isAdmin:
+        try:
+            with conn.cursor() as cur:
+                response = cur.execute('DELETE FROM `booking` WHERE `id`=%s', (booking_id))
+                conn.commit()
+        except Exception as e:
+            return 'Admin Cancel Fail: ' + e.message
+        if response == 0:
+            return 'Admin Cancel Fail'
+        return 'Admin Cancel Success'
+    else:
+        try:
+            with conn.cursor() as cur:
+                response = cur.execute('DELETE FROM `booking` WHERE `user_id` = %s and `id`=%s',(user_id, booking_id))
+                conn.commit()
+        except Exception as e:
+            return 'Cancel Fail: ' + e.message
+        if response == 0:
+            return 'Cancel Fail'
+        return 'Cancel Success'
+
+def connect_database():
     try:
         conn = pymysql.connect(db_host, user=db_username, passwd=db_password, db=db_name, connect_timeout=5)
     except Exception as e:
         conn = pymysql.connect()
-        print ("ERROR: " + e)
+        print (e)
     print("SUCCESS: Connection to RDS mysql instance succeeded")
-    with conn.cursor() as cur:
-        cur.execute("select subject_area, catalog_number, class_section, \
-                    start_time, end_time, lesson_date.ldate, venue.name, lecturer.name \
-                    from lesson left join lesson_date on lesson.id = lesson_date.lesson_id \
-                    left join venue on lesson.venue_id = venue.id\
-                    left join lesson_lecturer on lesson.id = lesson_lecturer.lesson_id\
-                    left join lecturer on lesson_lecturer.lecturer_id = lecturer.id\
-                    where location = '{}' and ldate= '{}'".format(venue_name,date))
-    for row in cur:
-        result.append({'subject_area':row[0],'catalog': row[1],'class': row[2],'start_time':str(row[3]), 'end_time':str(row[4]), 'date':str(row[5]), 'venue_name':row[6],'lecturer':row[7]})
-    return json.dumps(result)
+    return conn
